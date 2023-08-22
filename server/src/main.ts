bc590fd18326e4725cdfdcb7b5a3eb017767f589
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-08-22 01:13:12 -0400

    build(config): add cors and ansi config

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+
+  const port = process.env.PORT;
+  const coloredPort = `\x1b[33m\x1b[1m${port}\x1b[0m`;
+  const coloredText = `\x1b[32m\x1b[1m]> Server running on port\x1b[0m`;
+  const message = `${coloredText} ${coloredPort}`;
+
+  await app.listen(port);
+  console.log(message);
 }
+
 bootstrap();
