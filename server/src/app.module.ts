c501c585f1a2c030fdaf98ffe91822b88a7531a1
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-08-23 14:58:35 -0300

    Revert "Thomas/local env"

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,12 +6,11 @@ import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { InitiativesModule } from './initiatives/initiatives.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule, UsersModule, AuthModule, InitiativesModule],
+    PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- server/src/initiatives/dto/update-initiative.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInitiativeDto } from './create-initiative.dto';
-
-export class UpdateInitiativeDto extends PartialType(CreateInitiativeDto) {}
