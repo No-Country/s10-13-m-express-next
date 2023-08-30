83de72933644383e55ff96cba8a0cdcc05494e1b
davidmedev222 <davidmedev@gmail.com>
2023-08-29 23:07:34 -0300

    chore(ui): add UI of the middle wireframes of the config page

---
+++ client/src/components/icons/LogoutIcon.tsx
@@ -0,0 +1,17 @@
+function LogoutIcon() {
+  return (
+    <svg width='48' height='48' viewBox='0 0 48 48' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <mask id='mask0_409_3597' maskUnits='userSpaceOnUse' x='0' y='0' width='48' height='48'>
+        <rect width='48' height='48' fill='#D9D9D9' />
+      </mask>
+      <g mask='url(#mask0_409_3597)'>
+        <path
+          d='M9 41.0747C8.2 41.0747 7.5 40.7741 6.9 40.1728C6.3 39.5716 6 38.8701 6 38.0685V8.00622C6 7.20456 6.3 6.50311 6.9 5.90187C7.5 5.30062 8.2 5 9 5H23.95V8.00622H9V38.0685H23.95V41.0747H9ZM33.3 31.8055L31.15 29.651L36.25 24.5405H18V21.5342H36.15L31.05 16.4237L33.2 14.2692L42 23.0875L33.3 31.8055Z'
+          fill='#1C1B1F'
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default LogoutIcon
