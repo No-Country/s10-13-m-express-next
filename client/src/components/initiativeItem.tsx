3af49caaa1f4023e9e8f30eead9d4541cdcca6b4
Luna Catrillón <76113738+YosstinCode@users.noreply.github.com>
2023-08-31 07:05:53 -0500

    refactor(initiative): improve design consistency, props; update redirection
    Enhanced 'initiativeItem' component's design and props. Adjusted redirection with ID.

--- client/src/components/initiativeItem.tsx
+++ client/src/components/initiativeItem.tsx
@@ -1,37 +1,55 @@
 'use client'
 import Image from 'next/image'
+import categoryIcon from '@/../public/icon/category.svg'
+import starIcon from '@/../public/icon/star.svg'
+import locationIcon from '@/../public/icon/location_on.svg'
 import { useRouter } from 'next/navigation'
 
-type Props = {
+interface initiativeProps {
+  id: number
+  key: number
   title: string
   image: string
   location: string
-  minWidth?: string
+  categories: string[]
+  reviews: number
 }
 
-export default function InitiativeItem({ title, image, location, minWidth = '' }: Props) {
+const InitiativeItem = ({ image, title, location, categories, reviews, id }: initiativeProps) => {
   const router = useRouter()
 
   const handleClick = () => {
-    router.push('/initiatives/1')
+    router.push(`/initiatives/${id}`)
   }
-
   return (
-    <div className={`${minWidth} flex w-full cursor-pointer flex-col`} onClick={handleClick}>
-      <div className='flex w-full flex-col gap-2 '>
-        <div className='l relative aspect-[1/1]  w-full'>
-          <Image src={image} fill alt='Vercel Logo' className='aspect-[3/4] rounded-2xl object-cover' />
-        </div>
-        <div className='flex flex-col gap-2'>
-          <div>
-            <h1 className='bodyText font-medium'>{title}</h1>
-            <p className='text-sm font-light'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pellentesque dictum commodo. Curabitur purus
-              quam
-            </p>
-          </div>
-        </div>
+    <article
+      className=' bg-whiteSecondary flex min-h-[13.125rem] min-w-[10.375rem] flex-col gap-y-3 rounded-2xl px-3 py-4 shadow-[0px_2px_6px_0px_#B0B0B0] cursor-pointer'
+      onClick={handleClick}
+    >
+      <Image className='aspect-video min-h-24 min-w-36 rounded-lg' src={image} alt={`Image ${title}`} width={142} height={92} />
+      <div className='flex flex-col gap-2 w-36'>
+        <h3 className='text-base font-bold leading-tight text-black truncate'>{title}</h3>
+        <ul>
+          <li className='flex gap-1'>
+            <Image src={locationIcon} alt='location icon' />
+            <span className='block truncate w-32'>
+              {location}
+            </span>
+          </li>
+          <li className='flex gap-1'>
+            <Image src={categoryIcon} alt='category icon' />
+            <span className='block truncate w-32'>
+              {categories[0]}
+            </span>
+          </li>
+          <li className='flex gap-1'>
+            <Image src={starIcon} alt='star icon' />
+            {reviews}
+          </li>
+        </ul>
       </div>
-    </div>
+    </article>
   )
 }
+
+export default InitiativeItem
