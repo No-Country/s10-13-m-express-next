861f5f753f02075d3aee184c2eaf25131f5cb378
davidmedev222 <davidmedev@gmail.com>
2023-08-31 00:42:26 -0300

    feat(ui): add UI of the configuration page

--- client/src/app/(user)/account/components/config/ConfigOptions.tsx
+++ client/src/app/(user)/account/components/config/ConfigOptions.tsx
@@ -1,36 +1,43 @@
-import { DonationIcon, EditProfileIcon, LogoutIcon, NominationIcon, NotificationIcon } from '@/components'
+import { DonationIcon, EditProfileIcon, ErrorIcon, LogoutIcon, NominationIcon, NotificationIcon } from '@/components'
 import Link from 'next/link'
 
 function ConfigOptions() {
   return (
-    <ul className='mx-auto max-w-screen-xl text-lg text-blue-700'>
-      <li>
-        <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
-          <EditProfileIcon /> Editar Perfil
-        </Link>
-      </li>
-      <li>
-        <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
-          <NominationIcon /> Mis postulaciones
-        </Link>
-      </li>
-      <li>
-        <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
-          <DonationIcon /> Mis donaciones
-        </Link>
-      </li>
-      <li>
-        <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
-          <NotificationIcon /> Notificaciones
-        </Link>
-      </li>
-      <li>
-        <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
-          <LogoutIcon /> Cerrar sesión
-        </Link>
-      </li>
-      <button className='w-full cursor-pointer px-3 py-4 text-center text-red-500'>Eliminar cuenta</button>
-    </ul>
+    <>
+      <ul className='text-lg text-blue-700'>
+        <li>
+          <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
+            <EditProfileIcon className='fill-blue-700' /> Editar Perfil
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
+            <NominationIcon className='fill-blue-700' /> Mis postulaciones
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
+            <DonationIcon className='fill-blue-700' /> Mis donaciones
+          </Link>
+        </li>
+        <li>
+          <Link href='#' className='flex items-center gap-x-3 px-3 py-4'>
+            <NotificationIcon className='fill-blue-700' /> Notificaciones
+          </Link>
+        </li>
+        <li className='mx-auto my-5 h-[1px] w-48 bg-gray-500' />
+      </ul>
+      <div className='mt-auto text-lg text-blue-700'>
+        <button className='flex items-center gap-x-3 px-3 py-4'>
+          <LogoutIcon className='fill-blue-700' /> Cerrar sesión
+        </button>
+        <div className='mx-auto my-5 h-[1px] w-48 bg-gray-500' />
+        <button className='flex items-center gap-x-3 px-3 py-4 uppercase text-red-500'>
+          <ErrorIcon className='fill-red-500' />
+          Eliminar cuenta
+        </button>
+      </div>
+    </>
   )
 }
 
