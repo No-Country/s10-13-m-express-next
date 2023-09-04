f4519163d83f716c99e4a65959b145172b8fecd3
Juan Fernandez <jmf798@gmail.com>
2023-09-04 11:37:08 -0300

    docs: update-dto docs

--- server/src/initiatives/dto/update-initiative.dto.ts
+++ server/src/initiatives/dto/update-initiative.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateInitiativeDto } from './create-initiative.dto';
 
 export class UpdateInitiativeDto extends PartialType(CreateInitiativeDto) {}
