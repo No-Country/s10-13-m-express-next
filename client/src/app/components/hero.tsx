58ce85819fe5004908862bc2b4a4ff1e2d434bce
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-09-05 01:11:48 -0300

    style: prettier

--- client/src/app/components/hero.tsx
+++ client/src/app/components/hero.tsx
@@ -5,7 +5,7 @@ export default function HeroSec() {
   return (
     <section className='flex w-full items-center justify-center'>
       <div className='container flex flex-col gap-4'>
-      <h2 className='font-normal text-xl text-blue-600'>Buscar Iniciativas</h2>
+        <h2 className='text-xl font-normal text-blue-600'>Buscar Iniciativas</h2>
         <div className='flex flex-col gap-2'>
           <SearchInput placeholder='Buscar' />
           <SearchInput placeholder='Ubicacion' />
