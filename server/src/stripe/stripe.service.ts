5f077198f972aadc70f8b721cbf020f855dde424
cahov <christian.herrera.onaindi@gmail.com>
2023-09-05 14:35:04 -0400

    feat: Add prisma donation model

--- server/src/stripe/stripe.service.ts
+++ server/src/stripe/stripe.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import Stripe from 'stripe';
+
+const stripeApiKey = process.env.STRIPE_SECRET_KEY;
+const stripe = new Stripe(stripeApiKey, {
+    apiVersion: '2023-08-16',
+  });
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+    constructor(private readonly prisma: PrismaService){}
+
+    async createPaymentIntent(amount: number, userId: string){
+        const convertAmount = amount * 100 //El monto debe convertirse en centavos antes de enviarse a Stripe
+        const PaymentIntent = await stripe.paymentIntents.create({
+            amount: convertAmount,
+            currency: 'usd',
+            automatic_payment_methods: {
+                enabled: true,
+            },
+            metadata:{
+                userId: userId
+            }
+        })
+        return PaymentIntent;
+    }
+}
