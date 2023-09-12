2a80ed31e4d047249797ab5ba9e8be5bdcb33063
davidmedev222 <davidmedev@gmail.com>
2023-09-12 20:21:54 -0300

    fix(ui): fix UI of the home page

--- client/src/components/buttons/ButtonLink.tsx
+++ client/src/components/buttons/ButtonLink.tsx
@@ -5,10 +5,11 @@ interface Props extends LinkProps {
   children: React.ReactNode
   variant?: 'primary' | 'secondary'
   iconLeft?: JSX.Element
+  iconRight?: JSX.Element
   align?: 'center' | 'bottom'
 }
 
-function ButtonLink({ children, variant, iconLeft, align, ...rest }: Props) {
+function ButtonLink({ children, variant, iconLeft, iconRight, align, ...rest }: Props) {
   const classes = {
     link: clsx(
       'rounded-full px-6 py-2 text-center text-lg font-bold text-blue-500 transition-colors duration-300 hover:text-blue-300 ',
@@ -16,6 +17,7 @@ function ButtonLink({ children, variant, iconLeft, align, ...rest }: Props) {
       variant === 'secondary' &&
         'border border-solid border-blue-500 text-blue-700 shadow-md shadow-gray-500 hover:bg-blue-100',
       iconLeft && 'flex items-center justify-center gap-x-2',
+      iconRight && 'flex items-center justify-center gap-x-2',
       align === 'center' && 'mx-auto',
       align === 'bottom' && 'mt-auto'
     )
@@ -25,6 +27,7 @@ function ButtonLink({ children, variant, iconLeft, align, ...rest }: Props) {
     <Link {...rest} className={classes.link}>
       {iconLeft && iconLeft}
       {children}
+      {iconRight && iconRight}
     </Link>
   )
 }
--- client/src/components/initiativeItem.tsx
+++ client/src/components/initiativeItem.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 
 interface Props {
   initiative: any
-  minWidth?: string
 }
 
 interface ItemProps {
@@ -18,30 +17,36 @@ function Item({ imageSrc, text, icon = true }: ItemProps) {
   const textStyle = icon ? 'text-gray-800' : 'text-pink-500'
   return (
     <div className='flex gap-2'>
-      {icon && <Image src={imageSrc} width={10} height={13} alt='icon' />}
+      {icon && <Image src={imageSrc} priority width={10} height={13} sizes='40vw' className='h-3 w-3' alt='icon' />}
       <p className={`text-xs font-normal ${textStyle}`}>{text}</p>
     </div>
   )
 }
 
-export default function InitiativeItem({ initiative, minWidth = '' }: Props) {
+export default function InitiativeItem({ initiative }: Props) {
   return (
-    <Link href={`${Routes.INITIATIVES}/${initiative?.id}`}>
-      <div className={`${minWidth} flex w-full cursor-pointer flex-col rounded-lg shadow-initiativeItem`}>
-        <div className='flex w-full flex-col gap-2 p-3 '>
-          <div className='l relative aspect-[1/1]  w-full'>
-            <Image src={initiative.thumbnail} fill alt='thumbnail' className='aspect-[3/4] rounded-lg object-cover' />
-          </div>
-          <div className='flex flex-col gap-2'>
-            <h1 className='bodyText font-semibold'>{initiative.title}</h1>
-            <div className='flex flex-col gap-2'>
-              <Item imageSrc='' text='Playas felices' icon={false} />
-              <Item imageSrc='/icon/location_on.svg' text='Villa Gesell, Argentina' />
-              <Item imageSrc='/icon/category.svg' text='Medioambiente' />
-              <Item imageSrc='/icon/family_link.svg' text='Actividades Ecológicas' />
-              <Item imageSrc='/icon/star.svg' text='4.5' />
-            </div>
-          </div>
+    <Link
+      className='flex min-w-[240px] flex-col gap-2 rounded-lg p-3 shadow-initiativeItem'
+      href={`${Routes.INITIATIVES}/${initiative?.id}`}
+    >
+      <div className='relative aspect-square h-36'>
+        <Image
+          src={initiative.thumbnail}
+          priority
+          sizes='40vw'
+          fill
+          alt='thumbnail'
+          className='rounded-lg object-cover'
+        />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <h1 className='bodyText truncate font-semibold'>{initiative.title}</h1>
+        <div className='flex flex-col gap-2'>
+          <Item imageSrc='' text='Playas felices' icon={false} />
+          <Item imageSrc='/icon/location_on.svg' text='Villa Gesell, Argentina' />
+          <Item imageSrc='/icon/category.svg' text='Medioambiente' />
+          <Item imageSrc='/icon/family_link.svg' text='Actividades Ecológicas' />
+          <Item imageSrc='/icon/star.svg' text='4.5' />
         </div>
       </div>
     </Link>
