e5bb81478b9927cc897b452baa05eb65bf755f79
demattei-teo <teitodematteio@gmail.com>
2023-09-13 10:22:22 -0300

    feat: add username page

--- client/src/app/(user)/[username]/page.tsx
+++ client/src/app/(user)/[username]/page.tsx
@@ -1,23 +1,9 @@
-import store from '@/redux/store'
-import { currentUsersApi } from '@/redux/services/users.service'
-import Volunteer from './components/volunteer'
-import Organization from './components/organization'
-
-async function getUser(username: string) {
-  const userRequest = currentUsersApi.endpoints.getUsersByUsername.initiate(username)
-  const { data } = await store.dispatch(userRequest)
-  return data.user
-}
+import Content from './components/content'
 
 interface Props {
   params: { username: string }
 }
 
 export default async function Page({ params }: Props) {
-  const user = await getUser(params.username)
-  return (
-    <div className='flex min-h-screen w-screen flex-col'>
-      {user.role === 'volunteer' ? <Volunteer user={user} /> : <Organization user={user} />}
-    </div>
-  )
+  return <Content name={params.username} />
 }
