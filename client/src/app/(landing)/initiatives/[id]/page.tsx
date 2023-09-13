dda8dff31f478920b2c88a1a9e64f88a009384bc
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-09-13 17:03:15 -0300

    refactor: replace getInitative by rtkQuery hook and integrate Skeleton

--- client/src/app/(landing)/initiatives/[id]/page.tsx
+++ client/src/app/(landing)/initiatives/[id]/page.tsx
@@ -1,8 +1,17 @@
+'use client'
+import { useGetInitiativesByIdQuery } from '@/redux/services/initiatives.service'
 import type { Metadata } from 'next'
-import { Cover, Description, InitiativeInfo, Labels, Photos, Publications, Volunteers } from './components'
-import { initiativesApi } from '@/redux/services/initiatives.service'
-import store from '@/redux/store'
-import Review from './components/review'
+import {
+  Cover,
+  Description,
+  InitiativeInfo,
+  Labels,
+  Photos,
+  Publications,
+  Review,
+  Skeleton,
+  Volunteers
+} from './components'
 
 export const metadata: Metadata = {
   title: 'Iniciativa Individual',
@@ -16,25 +25,18 @@ interface Props {
   }
 }
 
-async function getInitiative(username: string) {
-  const initiativeRequest = initiativesApi.endpoints.getInitiativesById.initiate(username)
-  try {
-    const { data } = await store.dispatch(initiativeRequest)
-    return data
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function InitiativeDetailPage(props: Props) {
+function InitiativeDetailPage(props: Props) {
   const { params } = props
   const pandaImg =
     'https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80'
   const ostrich =
     'https://images.unsplash.com/photo-1682687220945-922198770e60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
 
-  const initiative = await getInitiative(params.id)
-  if (initiative === undefined) {
+  const { data: initiative, isLoading } = useGetInitiativesByIdQuery(params.id)
+
+  if (isLoading) return <Skeleton />
+
+  if (!initiative) {
     return (
       <div>
         <h3>La Iniciativa no existe</h3>
@@ -60,7 +62,7 @@ async function InitiativeDetailPage(props: Props) {
 
   return (
     <div className='flex w-full flex-col py-7'>
-      <Cover title={title} owner={owner.orgName as string} coverPhoto={thumbnail} />
+      <Cover title={title} owner={owner.orgName ?? ''} coverPhoto={thumbnail} />
       <Labels
         labels={{
           location: `${province}, ${country}`,
