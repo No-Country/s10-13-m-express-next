a831f624f8d72b8ef022ff2d3ae4d3530b9e33c6
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-09-13 16:57:09 -0300

    style: add x padding to InitiativesLayout

--- client/src/app/(landing)/initiatives/layout.tsx
+++ client/src/app/(landing)/initiatives/layout.tsx
@@ -1,3 +1,3 @@
 export default function InitiativesLayout({ children }: { children: React.ReactNode }) {
-  return <main className='px-7'>{children}</main>
+  return <main className='px-7 md:px-32 xl:px-96'>{children}</main>
 }
